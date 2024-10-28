446e9031f7cdbbc54fd8b95c4ea10644211ea937
Vojtěch Perník <develop@pervoj.cz>
2024-10-28 17:02:55 +0100

    feat: enhance styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ const users = lightdm.users ?? [];
 function App() {
   return (
     <div className="w-screen h-screen fixed inset-0 dark bg-neutral-900 text-neutral-100 grid place-content-center">
-      <div className="grid gap-3">
+      <div className="grid gap-3 w-full max-w-96 px-4">
         <h1 className="text-center">Users</h1>
         <div className="grid gap-3">
           {users.map((user) => (
             <div
               key={user.username}
-              className="p-3 rounded-xl bg-white/10 hover:bg-white/20 transition-colors grid grid-cols-[auto_1fr] gap-2 items-center w-48"
+              className="p-3 rounded-xl bg-white/10 hover:bg-white/20 transition-colors grid grid-cols-[auto_1fr] gap-2 items-center"
             >
               <img
                 src={user.image}
